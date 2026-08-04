5e63cd4a1dd3ce04dc53fca80b3f9e6657b714a0
Narayan438 <abcnames@gmail.com>
2026-08-04 13:15:56 +0000

    Initial reusable school template structure

---
+++ js/school-config.js
@@ -0,0 +1,23 @@
+const SCHOOL={
+
+nameNp:"श्री माइकोट माध्यमिक विद्यालय",
+
+nameEn:"Shree Maikot Secondary School",
+
+address:"बारपाक सुलिकोट-६, ताकुकोट, गोरखा",
+
+established:"२०१४",
+
+phone:"",
+
+email:"",
+
+website:"",
+
+logo:"assets/images/logo/logo.png",
+
+themeColor:"#174f9b",
+
+accent:"#d62d20"
+
+};
